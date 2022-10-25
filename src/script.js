f9e051aeda78d5ef6620b1ef2648c876ba0783bd
Apolo Garcia-Herrera <77506622+1aggarcia@users.noreply.github.com>
2022-10-25 13:59:47 -0700

    Auto ticker, restyled grid

--- src/script.js
+++ src/script.js
@@ -1,29 +1,26 @@
-const ROW = "r";
-const COL = "c";
 const WIDTH = 20;
 const HEIGHT = 15;
+const SEPARATOR = ":";
+
+const SNAKE_CLASS = "s_block";
+const FOOD_CLASS = "f_block";
 
 function main() {
     build_grid(WIDTH, HEIGHT);
-
-    let snake = new Snake();
-    let tick_btn = document.getElementById("tick_btn")
-    let dir_btn = document.getElementsByClassName("dir_btn");
-
-    tick_btn.onclick = function() { snake.tick(); }
-    dir_btn.onclick = function() { snake.redirect(dir_btn.id); }
+    run_snake();
     
 }
 
+// Fill HTML of table
 function build_grid(width, height) {
     let table = document.getElementById("grid");
     let id;
     let html = "";
 
     for (let r = 1; r < height+1; r++) {
-        html += "<tr>\n";
+        html += "<tr id ='" + r + "'>\n";
         for (let c = 1; c < width+1; c++) {
-            id = ROW + r + COL + c;
+            id = r + SEPARATOR + c;
             html += "<td id ='" + id + "'></td>\n";
         }
         html += "</tr>"
@@ -32,5 +29,67 @@ function build_grid(width, height) {
     table.innerHTML = html;
 
 }
+
+// Function holding snake vars
+async function run_snake() {
+    let blocks = ["7:10", "8:10", "9:10"];
+
+    let direction = [0, 1] // encoded as [downShift, leftShift]
+    let eating = true;
+    let alive = true;
+
+    for (let i = 0; i < blocks.length; i++) {
+        fill_block(blocks[i], true);
+    }
+    fill_block("7:11", false);
+
+    for (let i = 0; i < 9; i++) {
+        await delay(250);
+        blocks = tick(blocks, direction, eating); // move head, add to block array
+
+        // clear tail if not eating
+        if (eating == false) {
+            let tail = blocks.pop();
+            clear_block(tail);
+        } else {
+            eating = false;
+        }
+    }
+}
+
+// Fill block with snake
+function fill_block(id, isSnake) {
+    let block = document.getElementById(id);
+    if (isSnake) {
+        block.className = SNAKE_CLASS;
+    } else {
+        block.className = FOOD_CLASS;
+    }
+}
+
+// Remove snake from block
+function clear_block(id) {
+    document.getElementById(id).className = "";
+}
+
+// ticker
+function tick(blocks, direction, eating) {
+    let head = blocks[0];
+    let coords = head.split(":");
+
+    coords[0] = parseInt(coords[0]) + direction[0]; // Shift y down by direction[0]
+    coords[1] = parseInt(coords[1]) + direction[1]; // Shift x lefy by direction[1]
+
+    head = coords[0] + SEPARATOR + coords[1];
+    blocks = [head].concat(blocks);
+    fill_block(head, true);
+
+    return blocks;
+}
+
+// Delay time
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+  }
 
 main();
--- src/snake.js
+++ src/snake.js
@@ -1,20 +1,4 @@
-const BLOCK_CLASS = "s_block";
-
-class Snake {
-    constructor() {
-        this.blocks = ["r7c10", "r8c10", "r9c10"];
-        this.len = this.blocks.length;
-
-        this.down = 0;
-        this.left = 1;
-        this.eating = true;
-
-        for (let i = 0; i < this.len; i++) {
-            let block = document.getElementById(this.blocks[i]);
-            block.className = BLOCK_CLASS;
-        }
-    }
-
+/*
     redirect(id) {
         switch (String(id)) {
             case "left":
@@ -33,27 +17,4 @@ class Snake {
         console.log(this.down);
         console.log(this.left);
     }
-
-    tick() {
-        let head = this.blocks[0].substring(1);
-        let coords = head.split("c");
-
-        coords[0] = parseInt(coords[0]) + this.down;
-        coords[1] = parseInt(coords[1]) + this.left;
-
-        head = ROW + coords[0] + COL + coords[1];
-
-        this.blocks = [head].concat(this.blocks);
-        document.getElementById(head).className = BLOCK_CLASS;
-
-        if (this.eating == false) {
-            let tail = this.blocks.pop();
-            document.getElementById(tail).className = "";
-        } else {
-            this.len += 1;
-            this.eating = false;
-        }
-
-        console.log(this.blocks);
-    }
-}
+*/
